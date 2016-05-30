2ff5003745d4b27d333fb1f9322441dc198f3341
rcastano <rcastano@utp.edu.co>
2016-05-30 11:12:26 -0500

    Reorganizacion de codigo, firmas de procesos listeningThread y petitionThread, cambio de menu a chooseMenuAction, firma startServer debera inicializar los hilos

--- cliente.py
+++ cliente.py
@@ -1,8 +1,8 @@
 import socket
+import sys
+import threading
 
-# Definicion de Globales
-_MEMORYSIZE = 4
-
+#Memory Unit Class, It's the core of the shared memory system
 class MU:
 
 	def __init__(self):
@@ -37,26 +37,70 @@ class MU:
 			else:
 				print "I have no permissions to write this Memory Unit"
 
+	#Changes the permissions for the MU
 	def chmod(self, who, newPermissions):
 		if who == self.owner:
 			self.permissions = newPermissions
 		else:
 			print "You don't have enough rights to do this!"
 
+#-------------------------------------------------------------------------------------------
 
-
-
+# Definicion de Variables Globales
+_MEMORYSIZE = 4 											#Un maximo de 4 bloques
+peerSocket = "" 											#Socket Handler
+HOST = ''													#Host IP info 
+PORT = 10000												#Host Port info
+servData = (HOST,PORT)										#Tuple with Host Information, useful for the "UDP sending code" reading
 
 # La memoria es un diccionario de Unidades de Memoria (MU)
 memory = {}
 
-opciones = {
+def listeningThread():
+	while True:
+		recieved, server = peerSocket.recvfrom(4096)
+		print "data: " + recieved
+		if recibido == "close1qazxsw2":
+			peerSocket.close()
+			break
+
+# Acciones para el menu
+options = {
 	1:"Read data from memory",
 	2:"Write data into memory",
 	3:"Change permits of a data in memory",
 	4:"Create a data in memory",
 	5:"Exit",
 }
+
+# Devuelve la seleccion hecha en un menu de opciones
+def chooseMenuAction():
+	#Se inclina por hacer un ciclo while por que la recursividad podria no entregar la opcion correcta
+	while True:
+		print "Esto es lo que puede hacer aqui:"
+		for option in options:
+			print option, " - ", options[option]
+
+		print "\nPor favor seleccione la opcion que mas le apetezca"
+		selection = int(raw_input(">>> "))
+		if selection in options:
+			return selection
+		else:
+			print "Posiblemente algo este mal con su teclado"
+			print "Aparentemente ha escogido una opcion incorrecta"
+
+def petitionThread():
+	while True:
+		command = chooseMenuAction()
+		sent = peerSocket.sendTo(command, servData)
+		if command == "close":
+			break
+
+#Inicializa el servidor, su configuracion y los hilos que escuchan y envian mensajes
+def startServer():
+	peerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(servData)
+	#aqui se inicializan los hilos petition y listening
 
 # Cada nodo debe tener espacios de memoria disponibles (paginas) 
 # a los que cada uno de los nodos podra acceder de manera 
@@ -67,22 +111,3 @@ opciones = {
 # - Puede escribir un dato en la memoria Posicion de memoria, Tipo de dato, Contenido, Quien hace la peticion, y lo hace con los permisos de la pagina
 # - Cada nodo puede cambiar los permisos de sus paginas
 # - Cada nodo crear una pagina en su propia memoria
-
-def menu():
-	print "Esto es lo que puede hacer aqui:"
-	for opcion in opciones:
-		print opcion, " - ", opciones[opcion]
-
-	print "\nPor favor seleccione la opcion que mas le apetezca"
-	variable = int(raw_input(">>> "))
-	if variable in opciones:
-		return variable
-	else:
-		print "Posiblemente algo este mal con su teclado"
-		print "Aparetemente ha escogido una opcion incorrecta"
-		menu()
-
-print "hola, este es el cliente de memoria distribuida"
-print "realmente usted no sabe donde ni como se almacena la info"
-opcion = menu()
-print ("usted escogio: " + opciones[opcion])
