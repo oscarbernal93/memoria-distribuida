1a8e7ba985ef3aab955102bd1f05b763645cf907
Oscar Bernal <oscarbernal93@checkpoint>
2016-06-08 12:32:58 -0500

    avances muy significativos

--- cliente.py
+++ cliente.py
@@ -1,18 +1,27 @@
 import socket
 import sys
 import threading
+import re
+import string
 
 # Definicion de Variables Globales
-_MEMORYSIZE = 4 									#Un maximo de 4 bloques
-_PRIVATECONECTION = ("",12834)						#Tuple with Host Information, useful for the "UDP sending code" reading
-_PUBLICCONECTION = ("239.255.255.8",12835)			#Tuple with Host for Multicast Messaging
+_MEMORYSIZE = 1										#The max size of the memory
+_PRIVATEPORT = 12838								#Port to manage the private conection
+_PUBLICCONECTION = ("<broadcast>",12835)			#Tuple with Host for Multicast Messaging
+_PRIVATECONECTION = ("",_PRIVATEPORT)				#Tuple with Host Information, useful for the "UDP sending code" reading
 _DATASIZE = 2048									#Size of data 
-
+_TIMEOUT = 2										#Timeout in seconds of the connection
+# IP from the machine
+_LOCALIP = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 # Inicializacion de Socket
-privateSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-publicSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-privateSocket.bind(_PRIVATECONECTION)
+publicSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+publicSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+publicSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 publicSocket.bind(_PUBLICCONECTION)
+
+privateSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+privateSocket.settimeout(_TIMEOUT)
+privateSocket.bind(_PRIVATECONECTION)
 
 #-------------------------------------------------------------------------------------------
 
@@ -22,19 +31,19 @@ class MU:
 		self.datatype = "None"
 		self.content = ""
 		self.permissions = "1110"
-		self.owner = "127.0.0.1"
+		self.owner = _LOCALIP
 		self.accesses = []
 		self.restrictions = []
 
+	def __str__(self):
+		return "{"+"owner:"+self.owner+","+"datatype:"+self.datatype+","+"content:"+self.content+"}"
 	#Returns the content to the user which asked for it, depending on the permissions granted for "who"
 	def read(self, who):
 		if who != self.owner:
 			if (self.permissions[2] == "1"):
-				return self.content
-			else:
-				print "No permissions granted for this Memory Unit"
+				return self
 		else:
-			return self.content
+			return self
 
 	#Changes the content of the MU depending on the permissions granted for "who"
 	def write(self, who, NewDataType, newContent):
@@ -42,54 +51,80 @@ class MU:
 			if (self.permissions[3] == "1"):
 				self.content = newContent
 				self.datatype = NewDataType
-			else:
-				print "No permissions granted for this Memory Unit"
 		elif (self.permissions[1] == "1"):
 			self.content = newContent
 			self.datatype = NewDataType
-		else:
-			print "I have no permissions to write this Memory Unit"
 
 	#Changes the permissions for the MU
 	def chmod(self, who, newPermissions):
 		if who == self.owner:
 			self.permissions = newPermissions
-		else:
-			print "You don't have enough rights to do this!"
-
 
 # Controller Class, It's the master class, control everything
 class CC:
 	def __init__(self):
-		# La memoria es un diccionario de Unidades de Memoria (MU)
+		# The memory is a dictionary of Memory Units (MU)
 		self.memory = {}
 
 	def append(self, name, memUnit):
-		# Intento crearlo en la memoria local
-		if len(self.memory) < _MEMORYSIZE:
+		# put a memory unit in the memory
+		# First try to create in the local memory
+		if self.check():
 			self.memory[name]=memUnit
 		else:
-			# si la memoria esta llena pido al todos los nodos conectados disponibilidad de memoria
-			sent = publicSocket.sendTo("1n33dm3msp4c3", _PUBLICCONECTION)
-			recieved, node = privateSocket.recvfrom(_DATASIZE)
-			if recieved = "1h4v3m3msp4c3":
-				print "Somebody have space"
-				print node
+			# if the local memory is full, then send a message to all nodes
+			sent = publicSocket.sendto("1n33dm3msp4c3_"+str(_PRIVATEPORT), _PUBLICCONECTION)
+			recieved = ''
+			try:
+				recieved, node = privateSocket.recvfrom(_DATASIZE)
+			except Exception, e:
+				pass
+			
+			if recieved == "1h4v3m3msp4c3":
+				# Somebody have space
+				privateSocket.sendto(name+"_"+memUnit.owner,node)
 			else:
-				print "There is no memeory space available"
+				print "There is no memory space available"
 			pass
-			print "end"
 
-	def createMemUnit(self, who, name):
-		if len(self.memory) < _MEMORYSIZE:
-			newMemUnit = MU()
-			newMemUnit.owner = who;
-			self.memory[name]=newMemUnit
-		pass
+	def create(self, who, name):
+		#creates a new memory unit in the memory
+		newMemUnit = MU()
+		newMemUnit.owner = who;
+		self.append(name,newMemUnit)
+
+	def check(self):
+		# verify the available space in the local memory
+		return len(self.memory) < _MEMORYSIZE
+
+	def local_read(self, who, name):
+		if name in self.memory:
+			return self.memory[name].read(who)
+		else:
+			return None
+	def read(self, name):
+		# read a variable name from the memory
+		lr = self.local_read(_LOCALIP,name)
+		if lr is None:
+			#if the variable name isn't in the local memory ask in the broadcast
+			sent = publicSocket.sendto("r34dv4rpl34s3_"+str(_PRIVATEPORT)+"_"+name, _PUBLICCONECTION)
+			recieved = ''
+			try:
+				recieved, node = privateSocket.recvfrom(_DATASIZE)
+			except Exception, e:
+				pass
+			if recieved != '':
+				# Somebody have the data
+				return recieved
+			else:
+				return None;
+			pass
+		else:
+			return str(lr)
 
 #-------------------------------------------------------------------------------------------
 
-# Acciones para el menu
+# Actions available to the menu, remember if edit, do it too in the bottom of the code in the main loop
 optionsList = {
 	1:"Read data from memory",
 	2:"Write data into memory",
@@ -98,16 +133,41 @@ optionsList = {
 	5:"Exit",
 }
 
+# Messages Codes:
+# 1n33dm3msp4c3 = the sender need space in the memory
+# 1h4v3m3msp4c3 = the sender have space in the memory
+# r34dv4rpl34s3 = the sender need to read a memory
+
 def listeningThread():
-	'''
-	TODO: Matar el proceso desde afuera
 	while True:
-		recieved, server = peerSocket.recvfrom(4096)
-		print "data: " + recieved
-		if recibido == "close1qazxsw2":
-			peerSocket.close()
+		recieved, sender = publicSocket.recvfrom(4096)
+		if recieved == "close1qazxsw2":
 			break
-	'''
+		recieved = string.split(recieved,"_");
+		# asign the private port to sender tuple
+		sender = (sender[0],int(recieved[1]))
+		# save the recived data
+		data = recieved[0]
+		if data == '1n33dm3msp4c3':
+			if master.check():
+				privateSocket.sendto('1h4v3m3msp4c3',sender)
+				try:
+					recieved, node = privateSocket.recvfrom(_DATASIZE)
+					recieved = string.split(recieved,"_");
+					name = recieved[0]
+					owner = recieved[1]
+					master.create(owner,name)
+				except Exception, e:
+					pass
+			pass
+		if data == 'r34dv4rpl34s3':
+		# this mesage have a extra parameter
+			varname = recieved[2]
+			lr = master.local_read(sender[0],varname);
+			# if the var is in the local mem
+			if lr is not None:
+				privateSocket.sendto(str(lr),sender)
+				
 
 # Devuelve la seleccion hecha en un menu de opciones
 def chooseMenuAction():
@@ -134,30 +194,65 @@ def chooseMenuAction():
 			print "Possibly something is wrong with your keyboard"
 			print "Apparently you chose a wrong option, try again"
 
-#Inicializa el servidor, su configuracion y los hilos que escuchan y envian mensajes
-def startServer():
-	#aqui se inicializan los hilos petition y listening
-	tlist = threading.Thread(target=listeningThread)
-	tlist.start()
-	tlist.join()
-
-# Cada nodo debe tener espacios de memoria disponibles (paginas) 
-# a los que cada uno de los nodos podra acceder de manera 
-# transversal, aleatoria y transparente
-
-# Cada nodo puede hacer las siguientes acciones:
-# - Devolver un dato localizado en su propia memoria
-# - Puede escribir un dato en la memoria Posicion de memoria, Tipo de dato, Contenido, Quien hace la peticion, y lo hace con los permisos de la pagina
-# - Cada nodo puede cambiar los permisos de sus paginas
-# - Cada nodo crear una pagina en su propia memoria
-
-
-# Se imprime el encabezado de bienvenida
+# print a welcome message
 print "\n*********"
 print "*  DMC  *"
 print "*********\n"
 print "Hello, This is the Distributed Memory Client"
 print "You don't know how or where the info is stored"
-# Se Imprime el menu
-option = chooseMenuAction()
-print ("You choose: " + optionsList[option])
+# the master controller is created
+master = CC()
+
+# the listening thread is initializated
+tlist = threading.Thread(target=listeningThread)
+tlist.start()
+
+# Begins the MAIN LOOP
+while True:
+	option = chooseMenuAction()
+	print ("You choose: " + optionsList[option])
+	if option == 1:
+		#Read
+		print "\nPlease write the variable name"
+		name = raw_input(">>> ")
+		mu = master.read(name)
+		if mu is None:
+			print "The selected variable is inaccessible"
+		else:
+			print mu
+	if option == 2:
+		#Write
+		print "\nPlease write the variable name"
+		name = raw_input(">>> ")
+		print "\nPlease write the content type"
+		datatype = raw_input(">>> ")
+		print "\nPlease write the content"
+		content = raw_input(">>> ")
+		mu = master.write(name,datatype,content)
+		if mu is None:
+			print "The selected variable is inaccessible"
+		else:
+			print "new content of the variable: "+mu
+	if option == 3:
+		#ChMod
+		pass
+	if option == 4:
+		#Create Data
+		print "\nPlease write the variable name"
+		name = raw_input(">>> ")
+		print "\nPlease write the owner ip, you can write \"me\""
+		owner = raw_input(">>> ")
+		if owner == "me":
+			owner = _LOCALIP
+		pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+		if not pattern.match(owner):
+			print "Incorrect owner's ip address"
+		else:
+			master.create(owner,name)
+
+	if option == 5:
+		#Exit
+		publicSocket.sendto('close1qazxsw2',_PUBLICCONECTION);
+		publicSocket.close()
+		privateSocket.close()
+		break
